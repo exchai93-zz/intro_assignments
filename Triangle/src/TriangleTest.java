1e3f01ea0363bd89a0441283b6c378bd09ebd8ca
Emily Chai <echai93@gmail.com>
2018-01-04 14:22:08 +0000

    Configure JUNIT testing framework

---
+++ Triangle/src/TriangleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+
+    @Test
+
+
+}
---
+++ Triangle/src/TriangleTestDrive.java
@@ -0,0 +1,17 @@
+import static org.junit.junpiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+public class TriangleTestDrive {
+
+    @Test
+    public void
+
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+
+        System.out.print("Exercise 1: ");
+        assertEquals("*", triangle.printAsterisk());
+    }
+
+}
