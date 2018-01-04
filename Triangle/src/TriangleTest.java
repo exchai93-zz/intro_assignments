70da1f8c7711404608af4a0ee9cf0f749e296a01
Emily Chai <echai93@gmail.com>
2018-01-04 14:32:29 +0000

    Passing test for printing one asterisk to the console

--- Triangle/src/TriangleTest.java
+++ Triangle/src/TriangleTest.java
@@ -5,6 +5,12 @@ import static org.junit.Assert.*;
 public class TriangleTest {
 
     @Test
+    public void testPrintAsterisk() {
+        Triangle triangle = new Triangle();
+
+        String result = triangle.printAsterisk();
+        assertEquals("*", result);
+    }
 
 
 }
