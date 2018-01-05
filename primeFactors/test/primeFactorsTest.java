18864e7c0a91e6ad87f7b2ee1378a316dec76b53
Emily Chai <echai93@gmail.com>
2018-01-05 14:31:54 +0000

    Generate currently returns two prime factors

--- primeFactors/test/primeFactorsTest.java
+++ primeFactors/test/primeFactorsTest.java
@@ -8,7 +8,12 @@ public class primeFactorsTest {
 
     @Test
     public void generate_1isNone() {
-        assertThat(primeFactors.generate(1), is(ImmutableObservableList.generate()));
+        assertThat(primeFactors.generate(1), is(ImmutableList.generate()));
+    }
+
+    @Test
+    public void generate_2is2() {
+        assertThat(primeFactors.generate(2), is(ImmutableList.generate(2)));
     }
 
 }
