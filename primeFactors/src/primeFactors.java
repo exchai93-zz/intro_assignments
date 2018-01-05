9ad9fa3fbadf96591e87c06c36426e8adcc0e1c2
Emily Chai <echai93@gmail.com>
2018-01-05 14:27:24 +0000

    Need to fix tests

--- primeFactors/src/primeFactors.java
+++ primeFactors/src/primeFactors.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
+
+
 public class primeFactors {
 
+    public static ArrayList<Integer> generate(int n) {
 
+        ArrayList<Integer> factors = new ArrayList<>();
+
+        if (n > 1) {
+            factors.add(n);
+        }
+        return factors;
+    }
 }
--- primeFactors/test/primeFactorsTest.java
+++ primeFactors/test/primeFactorsTest.java
@@ -1,5 +1,14 @@
-import static org.junit.Assert.*;
+import com.sun.javafx.collections.ImmutableObservableList;
+import jdk.nashorn.internal.ir.annotations.Immutable;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.*;
 
 public class primeFactorsTest {
+
+    @Test
+    public void generate_1isNone() {
+        assertThat(primeFactors.generate(1), is(ImmutableObservableList.generate()));
+    }
 
 }
