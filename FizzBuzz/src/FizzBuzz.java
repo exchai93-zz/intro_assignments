2d1577c86d8415f203cf2b2c51c61e1e1118d682
Emily Chai <echai93@gmail.com>
2018-01-05 13:07:55 +0000

    Tests now pass

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -5,12 +5,13 @@ public class FizzBuzz {
             return "0";
 
         String s = "";
+
         if (i % 3 == 0)
             s = "Fizz";
         if (i % 5 == 0)
             s = "Buzz";
-        if (i % 3 == 0 && i % 5 == 0);
-        s = "FizzBuzz";
+        if (i % 3 == 0 && i % 5 == 0)
+            s = "FizzBuzz";
 
         return s.isEmpty() ? String.valueOf(i) : s;
     }
