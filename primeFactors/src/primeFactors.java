64d4632f226a09c716ee13e965c134fe39616da7
Emily Chai <echai93@gmail.com>
2018-01-05 14:38:05 +0000

    Convert while loop to a for

--- primeFactors/src/primeFactors.java
+++ primeFactors/src/primeFactors.java
@@ -4,15 +4,12 @@ import java.util.ArrayList;
 public class primeFactors {
 
     public static ArrayList<Integer> generate(int n) {
-
         ArrayList<Integer> factors = new ArrayList<>();
         int divisor = 2;
-        if (n > 1) {
-            while (n % divisor == 0) {
+
+        for (int divisor = 2; n > 1; divisor++)
+            for(; n % divisor == 0; n /= divisor)
                 factors.add(divisor);
-                n /= divisor;
-            }
-        }
         return factors;
     }
 }
